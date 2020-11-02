63ee9379ac3141d8e8bb92dfbd106ffc0ae4312b
guilherme cazaroto <gui.cazaroto@gmail.com>
2020-11-02 16:24:50 -0300

    [feature] composition api

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <hr>
+  <Composition />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
+import Composition from './components/CompositionApi'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Composition
   }
 }
 </script>
---
+++ src/components/CompositionApi.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="text-align: center">
+    <h1>{{name}}</h1>
+    <button @click="handleClick">Clique aqui</button>
+
+    <ul>
+      <li
+        v-for="user in users"
+        v-bind:key="user.name"
+      >
+        {{ user.name }} - {{ user.age }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const handleClick = () => console.log('Fui clicado')
+    const users = ref([])
+    users.value = [
+      { name: 'Julio', age: 18 },
+      { name: 'Marcell', age: 40 }
+    ]
+
+    return {
+      name: 'Guimarães',
+      handleClick,
+      users
+    }
+  }
+}
+</script>
