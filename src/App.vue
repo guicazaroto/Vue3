4acee8195b461c87e4c82780710f7150a959a158
guilherme cazaroto <gui.cazaroto@gmail.com>
2020-11-03 14:27:39 -0300

    [feature] counter composition

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <hr>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Composition /> -->
   <Composition />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import Composition from './components/CompositionApi'
+// import Composition from './components/CompositionApi'
+import Composition from './components/CompositionTwo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     Composition
   }
 }
---
+++ src/components/CompositionTwo.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="increment">
+    Count is: {{ count }} , double is: {{ double }}
+  </button>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+
+export default {
+  setup () {
+    const count = ref(1)
+    const double = computed(() => count.value * 2 )
+ 
+    function increment () {
+      count.value++
+    }
+
+    return { count, double, increment}
+  }
+}
+</script>
+
+<style>
+
+</style>
